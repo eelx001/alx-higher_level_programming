3eecc4b48ce89ba796392bc0fc43902fe7f09245
elyse502 <elyseniyibizi502@gmail.com>
2023-06-06 03:19:40 -0700

    Inserts a number into a sorted singly linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -7,19 +7,20 @@
 *
 * Return: Address of New Node OR NULL on failure.
 */
-listint_t *insert_node(listint_t **head, int num)
+listint_t *insert_node(listint_t **head, int number)
 {
 		listint_t *current = &head;
 		listint_t *next = current->next;
+
 		while (current->next != '\0')
 		{
-				if (next->n > num)
-				{
-						current->n = num;
-						current->next = current->next;
-						return (current);
-				}
-				current = current->next;
+			if (next->n > number)
+			{
+				current->n = number;
+				current->next = current->next;
+				return (current);
+			}
+			current = current->next;
 		}
 		return (NULL);
 }
