c7b3065927f12bac9652b7f76d9b05a3ad158971
elyse502 <elyseniyibizi502@gmail.com>
2023-07-02 23:42:13 -0700

    Prints a square with the character #

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module 4-print_square
+Contains method that prints square with #'s
+Takes in an int size
+"""
+
+"""Defines a square-printing function."""
+
+
+def print_square(size):
+    """Print a square with the # character.
+    Args:
+        size (int): The height/width of the square.
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
