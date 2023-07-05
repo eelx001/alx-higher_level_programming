52907abe919e01b00ce6475d9251c1a2fe41682a
elyse502 <elyseniyibizi502@gmail.com>
2023-07-05 02:59:11 -0700

    Defines a rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+"""
+Module 0-rectangle
+Contains class Rectangle
+Empty class
+"""
+
+
+class Rectangle():
+    """ Defines empty class rectangle """
+    pass
