197adb03e0d66d3c369a39c233c43d4539a58586
elyse502 <elyseniyibizi502@gmail.com>
2023-07-12 02:54:23 -0700

    script a class BaseGeometry (based on 5-base_geometry.py)

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+Contains empty class BaseGeometry
+with public instance method area
+"""
+
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
