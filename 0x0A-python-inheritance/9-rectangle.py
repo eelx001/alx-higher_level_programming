3f20eb2c39cced50cd0da0853be3014a19e25c95
elyse502 <elyseniyibizi502@gmail.com>
2023-07-12 03:10:13 -0700

    Inherits from BaseGeometry (7-base_geometry.py). (task based on 8-rectangle.py)

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module 9-rectangle
+Contains parent class BaseGeometry
+with public instance method area and integer_validation
+Contains subclass Rectangle
+with instantiation of private attributes width and height, validated by parent,
+extends parent's area method and prints with __str__
+"""
+
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Intialize a new Rectangle.
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+        """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the print() and str() representation of a Rectangle."""
+        string = "[" + str(self.__class__.__name__) + "] "
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
