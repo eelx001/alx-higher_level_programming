24d73d441551e84f5b9cf4e4716335e7be2836a8
elyse502 <elyseniyibizi502@gmail.com>
2023-07-12 02:51:57 -0700

    Script empty class BaseGeometry

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 5-base_geometry
+Contains empty class BaseGeometry
+"""
+
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
