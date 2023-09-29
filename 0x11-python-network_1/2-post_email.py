959d57b7a48e1a436aa9c02ab3b0716b53d9af05
NIYIBIZI Elysée <125453474+elyse502@users.noreply.github.com>
2023-09-29 09:56:11 +0200

    Takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,11 +4,16 @@
 Usage: ./2-post_email.py <URL> <email>
   - Displays the body of the response.
 """
-if __name__ == '__main__':
-    import sys
-    from urllib import request, parse
+import sys
+import urllib.parse
+import urllib.request
 
-    data = parse.urlencode({'email': sys.argv[2]}).encode()
-    req = request.Request(sys.argv[1], data=data)
-    with request.urlopen(req) as res:
-        print(res.read().decode('UTF-8'))
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
